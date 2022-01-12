1148beb66bb6dfa80b6a16899cc7f2028f293824
Aubis Sanchez <aubisst@gmail.com>
2022-01-12 03:48:32 -0400

    improves on inventory page but still with some issues, inventory at 80%.

--- components/InventoryCard/InventoryCard.js
+++ components/InventoryCard/InventoryCard.js
@@ -1,21 +1,27 @@
 import { PriceChange } from '@mui/icons-material'
+import Link from 'next/link'
 import React from 'react'
 
 
 const InventoryCard = ({ product }) => {
 
+    const { permalink } = product;
     return (
-        <button className='bg-gray-200 border-2 rounded-md h-72  w-60 hover:shadow-md hover:brightness-110  duration-300 ease-in'>
-            <div className='h-3/4 w-full hover:brightness-100'>
-                    <img src={ product.image.url } alt={ product.name } className='w-full h-full object-cover p-4  bg-gray-300 group-hover:scale-105 ease-in-out duration-300 shadow-md hover:shadow-lg'/> 
+        <Link 
+            href={`/products/${permalink}`}
+        >
+            <a className='bg-gray-300 border-2 rounded-md h-72  w-60 hover:shadow-md hover:brightness-110  duration-300 ease-in'>
+                <div className='h-3/4 w-full hover:brightness-100'>
+                        <img src={ product.image.url } alt={ product.name } className='w-full h-full object-cover p-4  bg-gray-300 group-hover:scale-105 ease-in-out duration-300 shadow-md hover:shadow-lg'/> 
+                </div>
+                
+            <div className=' h-1/5 w-full flex flex-col justify-center items-center'>
+                <p className='font-semibold text-blue-700'>{product.name}</p>
+                <p className='font-semibold text-gray-600'>{product.price.formatted_with_symbol}</p>
             </div>
-            
-           <div className=' h-1/5 w-full flex flex-col justify-center items-center'>
-            <p className='font-semibold text-blue-700'>{product.name}</p>
-            <p className='font-semibold text-gray-600'>{product.price.formatted_with_symbol}</p>
-           </div>
+            </a>
            
-        </button>
+        </Link>
     )
 }
 
--- pages/inventory.js
+++ pages/inventory.js
@@ -1,4 +1,4 @@
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faArrowCircleRight, faArrowCircleLeft, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useEffect, useState } from 'react'
 import InventoryCard from '../components/InventoryCard/InventoryCard';
@@ -7,7 +7,7 @@ import CircularProgress from '@mui/material/CircularProgress';
 
 
 export async function getStaticProps() {
-    const { data: products } = await commerce.products.list();
+    const { data: products } = await commerce.products.list({ limit: 40});
     const { data: categories } = await commerce.categories.list();
 
     return {
@@ -20,43 +20,100 @@ export async function getStaticProps() {
 
 const Inventory = ( {products, categories} ) => {
 
+    const initialMin = 0;
+    const initialMax = 11;
     const [searchValue, setSearchValue] = useState('')
     const [filteredProducts, setFilteredProducts] = useState(products)
-    const [isLoading, setIsLoading] = useState(false)
+    const [isLoading, setIsLoading] = useState(false);
+    const [resetCheckboxes, setResetCheckboxes] = useState(false)
+    const [page, setPage] = useState(1);
+    const [minIndex, setMinIndex] = useState(initialMin);
+    const [maxIndex, setMaxIndex] = useState(initialMax);
+    const [maxPage, setMaxPage] = useState();
+    
 
+    console.log(Math.ceil(filteredProducts.length / 12), page, maxPage,)
 
     useEffect(() => {
-        setIsLoading(true)
-        if( isLoading ){
-            setTimeout(() => {
+        // setIsLoading(true)
+        // if( isLoading ){
+        //     setTimeout(() => {
                 setFilteredProducts(products.filter( product => product.name.toLowerCase().includes(searchValue.toLowerCase())));        
-                setIsLoading(false)
-            }, 500);
+                // setIsLoading(false)
+          //  }, 500);
+        //}
+        setMaxPage( Math.ceil(filteredProducts.length / 12))
+    }, [setFilteredProducts,searchValue, ])
+
+
+    const handleCategory = async( e ) => {
+        const {data: newProducts} = await commerce.products.list({ category_slug: e.target.name.toLowerCase() })
+        
+        if( e.target.checked ){
+            const fid = filteredProducts.map( fp => fp.id );
+            newProducts.map( newProduct =>  !fid.includes(newProduct.id) && setFilteredProducts( prevState => [ ...prevState, newProduct ] )  )
+            setMaxPage( Math.ceil(filteredProducts.length / 12))
+            return
         }
-            
+        const npid = newProducts.map( np => np.id );
+        setFilteredProducts( filteredProducts.filter( fp => !npid.includes(fp.id) )  )
+        setMaxPage( Math.ceil(filteredProducts.length / 12))
 
-    }, [setFilteredProducts,searchValue])
-
-    // console.log(categories)
-
-    const handleCheckbox = ( e ) => {
-        console.log( e.target.name, e.target.checked)
     }
 
-    console.log(searchValue)
+
+    const handleSubcategory = async( e, slug, variant ) => {
+        
+        const {data: newSubProducts} = await commerce.products.list({ category_slug: slug })
+        const addProducts = newSubProducts.filter( np => np.categories[1].name === variant );
+        if( e.target.checked ){
+             const fid = filteredProducts.map( fp => fp.id );
+             addProducts.map( ap =>  !fid.includes(ap.id) && setFilteredProducts( prevState => [ ...prevState, ap ] )  ) 
+             setMaxPage( Math.ceil(filteredProducts.length / 12))
+             return
+        }
+         const npid = addProducts.map( np => np.id );
+         setFilteredProducts( filteredProducts.filter( fp => !npid.includes(fp.id) )  )
+         setMaxPage( Math.ceil(filteredProducts.length / 12))
+
+    }
+
+    const handleSearchInput = (e) => {
+        setPage(1);
+        setMaxPage( Math.ceil(filteredProducts.length / 12))
+        setMinIndex(initialMin)
+        setMaxIndex(initialMax)
+        setResetCheckboxes(true);
+        setSearchValue(e.target.value)
+        setResetCheckboxes(false)
+        
+    }
+
+    const handleNextPage = () => {
+        setMinIndex( minIndex + 12 )
+        setMaxIndex( maxIndex + 12 )
+        setPage( page + 1 )
+    }
+
+    const handlePrevPage = () => {
+        setMinIndex( minIndex - 12 )
+        setMaxIndex( maxIndex - 12 )
+        setPage( page - 1 )
+    }
 
     
     return (
         <div className='h-auto w-screen flex flex-row animate__animated animate__fadeInTop'>
-            <aside className='w-1/5  h-full bg-gray-200'>
+            <aside className='w-1/5  h-full bg-gray-300/80 font-text-1 text-blue-800 text-lg'>
                 <div className='flex flex-col justify-center items-center py-3'>
                     <label className='font-semibold text-lg'>Find Articles</label>
                     <input 
                         name='finder'
                         value={searchValue}
-                        onChange={ (e ) =>  setSearchValue( e.target.value )}
+                        autoComplete='off'
+                        onChange={ (e ) =>  handleSearchInput( e )}
                         placeholder='Brand or product name'
-                        className='py-4 px-2 border-2 border-slate-400 rounded-md mt-2'
+                        className='py-4 px-2 border-2 border-slate-400 rounded-md mt-2 focus:ring-violet-300'
                     />
                 </div>
                 {/* <div className='h-2 w-4/5 bg-gray-400 '></div> */}
@@ -69,7 +126,8 @@ const Inventory = ( {products, categories} ) => {
                                 <input 
                                     type='checkbox'
                                     name={category.name}
-                                    onChange={(e) => handleCheckbox(e)}
+                                    onChange={(e) => handleCategory(e)}
+                                
                                     
                                 />
                             
@@ -82,6 +140,7 @@ const Inventory = ( {products, categories} ) => {
                                         <input 
                                             type='checkbox'
                                             name={child.name}
+                                            onChange={(e) => handleSubcategory(e, category.name, child.name)}
                                             
                                         />
                                         <label className='pl-2'>{child.name}</label>              
@@ -94,18 +153,41 @@ const Inventory = ( {products, categories} ) => {
                     </div>
                 </div>
             </aside>
-            <div className='h-screen w-4/5'>
+            <div className='h-screen w-4/5 flex flex-col'>
                 <div className={`h-full w-4/5  m-auto ${!isLoading ? 'grid grid-cols-4 gap-4' : ''} mt-2 bg-white `}>
-                   {isLoading ? (
+                   {/* {isLoading ? (
                        <div className='w-full h-full flex justify-center items-center '>
                            <CircularProgress size='4rem' />
                        </div>
-                    ):
-                        filteredProducts.map( product => <InventoryCard key={product.id} product={product} isLoading={isLoading} setIsLoading={setIsLoading} /> )
-                   } 
+                    ): */}
+                        {filteredProducts.map( (product,index )=> (index >= minIndex && index<= maxIndex ) && <InventoryCard key={product.id} product={product} isLoading={isLoading} setIsLoading={setIsLoading} /> )}
+                   {/* }  */}
+                <div className='w-full h-12 k col-span-full '>
+                    <div className='w-full flex justify-center items-center '>
+                        {page !== 1 && (
+                            <button 
+                                className='h-full'
+                                onClick={handlePrevPage}
+                            >
+                                <span className='flex flex-row items-center mr-2 font-semibold text-gray-700'><FontAwesomeIcon icon={faArrowCircleLeft} size='2x' className='text-blue-600 mr-1' /> Prev Page</span>
+                            </button>
+                        ) }
+                        { page !== maxPage  && (
+                            <button 
+                                className='h-full'
+                                onClick={handleNextPage}
+                                >
+                                <span className='flex flex-row items-center ml-2 font-semibold text-gray-700'>Next Page<FontAwesomeIcon icon={faArrowCircleRight}  size='2x' className='text-blue-600 ml-1' /></span>
+                            </button>
+                        )  }
+                    </div>
                 </div>
-
+                
+                </div>
+               
             </div>
+         
+            
             
            
         </div>
